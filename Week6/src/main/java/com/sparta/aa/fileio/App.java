265dfb51d568115c4e76f97dc5da3a2d7501e8d8
Adam Alrasi <adam.alrasi1@gmail.com>
2024-03-25 11:59:18 +0000

    newBufferedReader

--- Week6/src/main/java/com/sparta/aa/fileio/App.java
+++ Week6/src/main/java/com/sparta/aa/fileio/App.java
@@ -1,7 +1,18 @@
 package com.sparta.aa.fileio;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
 
 public class App {
     public static void main(String[] args) {
-
+        Path input = Paths.get("src/main/java/com/sparta/aa/fileio/input.txt");
+        try {
+            BufferedReader reader = Files.newBufferedReader(input);
+        } catch (IOException e) {
+            System.out.println("File has not be found!");
+        }
     }
 }
